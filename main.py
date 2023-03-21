912b1f8032e7d79ad7224cc81de4eeac2dce3526
ghgrmws <chenyang.zhang.chn@outlook.com>
2023-03-21 11:52:27 +0800

    要一个函数一个函数的去摸了

--- main.py
+++ main.py
@@ -1,22 +1,35 @@
-import os
-from ortools.linear_solver import pywraplp
+import math
+
+from ortools.sat.python import cp_model
 from methods import get_data
 
 
 def resolve(file_path):
     N, L, W, H, boxes = get_data(file_path)
-    outer = pywraplp.Solver.CreateSolver('SAT')
-    b = outer.BoolVar('b')
+
+    outer = cp_model.CpModel()
+    bs = list()
+    vbs = list()  # volume of boxes
+    for i in range(N):
+        bs.append(outer.NewBoolVar('b' + str(i)))
+        vbs.append(math.prod(boxes[i]))
+
+    print(bs)
+    print(vbs)
+
+    outer_solver = cp_model.CpSolver()
+    outer_status = outer_solver.Solve(outer)
+    if outer_status == cp_model.OPTIMAL or \
+            outer_status == cp_model.FEASIBLE:
+        for b in bs:
+            print(b, "is", outer_solver.Value(b))
+    else:
+        print('No solution found.')
     return -1
 
 
 def main():
-    dirs = os.listdir("Data")
-    for file in dirs:
-        if file.endswith('.csv'):
-            result = resolve("Data\\" + file)
-            print(result)
-            break
+    result = resolve("Data\\" + "0.csv")
 
 
 if __name__ == '__main__':
--- test.py
+++ test.py
@@ -13,14 +13,14 @@ def resolve(file_path):
     for i in range(N):
         bs.append(outer.NewBoolVar('b' + str(i)))
         vbs.append(math.prod(boxes[i]))
-        print('b' + str(i))
 
-    print(boxes)
+    print(bs)
     print(vbs)
 
     outer_solver = cp_model.CpSolver()
     outer_status = outer_solver.Solve(outer)
-    if outer_status == cp_model.OPTIMAL or outer_status == cp_model.FEASIBLE:
+    if outer_status == cp_model.OPTIMAL or \
+            outer_status == cp_model.FEASIBLE:
         for b in bs:
             print(b, "is", outer_solver.Value(b))
     else:
@@ -32,15 +32,5 @@ def run():
     result = resolve("Data\\" + "0.csv")
 
 
-def test():
-    # no
-    # a = 'abcd'
-    # s = next(a)
-    # print(a)
-    #  哒咩
-    return 0
-
-
 if __name__ == '__main__':
     run()
-    # test()
