c2482c935229eb9370860e7b94475c432ff0e918
ghgrmws <chenyang.zhang.chn@outlook.com>
2023-03-21 16:35:37 +0800

    这都是些啥玩意儿？看不懂。谁来救我一下子啊

--- test.py
+++ test.py
@@ -8,23 +8,41 @@ def resolve(file_path):
     N, L, W, H, boxes = get_data(file_path)
 
     outer = cp_model.CpModel()
-    bs = list()
-    vbs = list()  # volume of boxes
+    L = outer.NewConstant(L)
+    W = outer.NewConstant(W)
+    H = outer.NewConstant(H)
+
+    b = list()
+    l = list()
+    w = list()
+    h = list()
     for i in range(N):
-        bs.append(outer.NewBoolVar('b' + str(i)))
-        vbs.append(math.prod(boxes[i]))
+        b.append(outer.NewBoolVar('b' + str(i)))
+        l.append(outer.NewConstant(boxes[i][0]))
+        w.append(outer.NewConstant(boxes[i][1]))
+        h.append(outer.NewConstant(boxes[i][2]))
 
-    print(bs)
-    print(vbs)
+    # outer.Add()
 
-    outer_solver = cp_model.CpSolver()
-    outer_status = outer_solver.Solve(outer)
-    if outer_status == cp_model.OPTIMAL or \
-            outer_status == cp_model.FEASIBLE:
-        for b in bs:
-            print(b, "is", outer_solver.Value(b))
-    else:
-        print('No solution found.')
+    for i in range(N):
+        outer.Maximize(b[i] * l[i] * w[i] * h[i])
+
+    while True:
+        outer_solver = cp_model.CpSolver()
+        outer_status = outer_solver.Solve(outer)
+
+        inner = cp_model.CpModel()
+
+        inner_bs = list()
+        if outer_status == cp_model.OPTIMAL or outer_status == cp_model.FEASIBLE:
+            for i in range(N):
+                if outer_solver.Value(b[i]):
+                    inner.NewBoolVar(str(b))
+                    inner_bs.append(b)
+        else:
+            print('No solution found in outer layer.')
+            break
+
     return -1
 
 
