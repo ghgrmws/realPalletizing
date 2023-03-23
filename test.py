2dac10c7be281e6787917bd53f3be1ebe0935625
ghgrmws <chenyang.zhang.chn@outlook.com>
2023-03-23 16:43:17 +0800

    我觉得我搞定了，虽然运行速度很慢

--- test.py
+++ test.py
@@ -1,4 +1,6 @@
 import math
+
+import numpy as np
 from ortools.linear_solver import pywraplp
 from ortools.sat.python import cp_model
 
@@ -61,6 +63,28 @@ from methods import get_data
 #     result = resolve_by_cp_model("Data\\" + "0.csv")
 
 
+def box_rotation(boxes):
+    ite = list()
+    i = 0
+
+    new_boxes = list()
+    for b in boxes:
+        s = set()
+        s.add((b[0], b[1], b[2]))
+        s.add((b[0], b[2], b[1]))
+        s.add((b[1], b[0], b[2]))
+        s.add((b[1], b[2], b[0]))
+        s.add((b[2], b[1], b[0]))
+        s.add((b[2], b[0], b[1]))
+        id = list()
+        for a in s:
+            new_boxes.append(list(a))
+            id.append(i)
+            i += 1
+        ite.append(id)
+    return ite, new_boxes
+
+
 def make_constraints(U, L, W, H):
     constraints = {
         'num_constraints': U * (L * W + L * H, W * H),
@@ -73,13 +97,49 @@ def make_constraints(U, L, W, H):
     return constraints
 
 
+def print_solution(solver, x, y, z, b, V):
+    for i in range(V):
+        print('x[%i] = %i' % (i, solver.Value(x[i])), end=', ')
+        print('y[%i] = %i' % (i, solver.Value(y[i])), end=', ')
+        print('z[%i] = %i' % (i, solver.Value(z[i])))
+    for i in range(V):
+        for j in range(i + 1, V):
+            for k in range(6):
+                print('b[%i][%i][%i] = %i' % (i, j, k, solver.Value(b[i][j][k])))
+
+
+def check_correctness(solver, x, y, z, b, v, V, W, H, D, data):
+    s = -np.ones((W + 1, H + 1, D + 1))
+    for i in range(V):
+        px = solver.Value(x[i])
+        py = solver.Value(y[i])
+        pz = solver.Value(z[i])
+        for w in range(data['width'][v[i]]):
+            for h in range(data['height'][v[i]]):
+                for d in range(data['depth'][v[i]]):
+                    # print(px, py, pz, data['width'][v[i]], data['height'][v[i]], data['depth'][v[i]])
+                    if s[px + w][py + h][pz + d] != -1:
+                        print("It's incorrect!")
+                    else:
+                        s[px + w][py + h][pz + d] = v[i]
+    # for i in range(V):
+    #     for j in range(i + 1, V):
+    #         for k in range(6):
+    #             print('b[%i][%i][%i] = %i' % (i, j, k, solver.Value(b[i][j][k])))
+    print("It's correct!")
+
+
 def resolve_by_linear_solver(file_path):
-    N, L, W, H, boxes = get_data(file_path)
+
+    N, W, H, D, boxes = get_data(file_path)
+
+    ites, boxes = box_rotation(boxes)
+    N = len(boxes)
 
     data = {
-        'length': [box[0] for box in boxes],
-        'width': [box[1] for box in boxes],
-        'height': [box[2] for box in boxes],
+        'width': [box[0] for box in boxes],
+        'height': [box[1] for box in boxes],
+        'depth': [box[2] for box in boxes],
     }
 
     outer = pywraplp.Solver.CreateSolver('SCIP')
@@ -90,13 +150,17 @@ def resolve_by_linear_solver(file_path):
         u[i] = outer.BoolVar('u[%i]' % i)
         arg_u[u[i]] = i
 
-    outer_constraint = outer.RowConstraint(0, L * W * H, '')
+    outer_constraint = outer.RowConstraint(0, W * H * D, '')
     for i in range(N):
-        outer_constraint.SetCoefficient(u[i], data['length'][i] * data['width'][i] * data['height'][i])
+        outer_constraint.SetCoefficient(u[i], data['width'][i] * data['height'][i] * data['depth'][i])
+    for ite in ites:
+        outer_constraint = outer.RowConstraint(0, 1, '')
+        for i in ite:
+            outer_constraint.SetCoefficient(u[i], 1)
 
     outer_objective = outer.Objective()
     for i in range(N):
-        outer_objective.SetCoefficient(u[i], data['length'][i] * data['width'][i] * data['height'][i])
+        outer_objective.SetCoefficient(u[i], data['width'][i] * data['height'][i] * data['depth'][i])
     outer_objective.SetMaximization()
 
     while True:
@@ -106,7 +170,7 @@ def resolve_by_linear_solver(file_path):
         V = 0  # the number of selected boxes in the outer layer
         outer_status = outer.Solve()
         if outer_status == pywraplp.Solver.OPTIMAL:
-            print('Outer objective value =', outer.Objective().Value())
+            # print('Outer objective value =', outer.Objective().Value())
             for i in range(N):
                 if u[i].solution_value() > 0:
                     v[V] = arg_u[u[i]]
@@ -126,9 +190,9 @@ def resolve_by_linear_solver(file_path):
 
         # every box should be totally in the space
         for i in range(V):
-            x[i] = inner.NewIntVar(0, L - data['length'][i], 'x[%i]' % i)
-            y[i] = inner.NewIntVar(0, W - data['width'][i], 'y[%i]' % i)
-            z[i] = inner.NewIntVar(0, H - data['height'][i], 'z[%i]' % i)
+            x[i] = inner.NewIntVar(0, W - data['width'][v[i]], 'x[%i]' % i)
+            y[i] = inner.NewIntVar(0, H - data['height'][v[i]], 'y[%i]' % i)
+            z[i] = inner.NewIntVar(0, D - data['depth'][v[i]], 'z[%i]' % i)
             for j in range(i + 1, V):
                 for k in range(6):
                     b[i][j].append(inner.NewBoolVar('b[%i][%i][%i]' % (i, j, k)))
@@ -139,12 +203,12 @@ def resolve_by_linear_solver(file_path):
 
         for i in range(V):
             for j in range(i + 1, V):
-                inner.Add(x[i] + data['length'][v[i]] - x[j] <= 0).OnlyEnforceIf(b[i][j][0])
-                inner.Add(x[j] + data['length'][v[j]] - x[i] <= 0).OnlyEnforceIf(b[i][j][1])
-                inner.Add(y[i] + data['width'][v[i]] - y[j] <= 0).OnlyEnforceIf(b[i][j][2])
-                inner.Add(y[j] + data['width'][v[j]] - y[i] <= 0).OnlyEnforceIf(b[i][j][3])
-                inner.Add(z[i] + data['height'][v[i]] - z[j] <= 0).OnlyEnforceIf(b[i][j][4])
-                inner.Add(z[j] + data['height'][v[j]] - z[i] <= 0).OnlyEnforceIf(b[i][j][5])
+                inner.Add(x[i] + data['width'][v[i]] - x[j] <= 0).OnlyEnforceIf(b[i][j][0])
+                inner.Add(x[j] + data['width'][v[j]] - x[i] <= 0).OnlyEnforceIf(b[i][j][1])
+                inner.Add(y[i] + data['height'][v[i]] - y[j] <= 0).OnlyEnforceIf(b[i][j][2])
+                inner.Add(y[j] + data['height'][v[j]] - y[i] <= 0).OnlyEnforceIf(b[i][j][3])
+                inner.Add(z[i] + data['depth'][v[i]] - z[j] <= 0).OnlyEnforceIf(b[i][j][4])
+                inner.Add(z[j] + data['depth'][v[j]] - z[i] <= 0).OnlyEnforceIf(b[i][j][5])
                 inner.AddBoolOr(b[i][j])
 
         xyz = {}
@@ -163,27 +227,21 @@ def resolve_by_linear_solver(file_path):
             outer_constraint = outer.RowConstraint(0, V - 1, '')
             for i in range(V):
                 outer_constraint.SetCoefficient(u[v[i]], 1)
-            # print('Number of constraints =', outer.NumConstraints())
+            print('Number of constraints =', outer.NumConstraints())
         else:
             print('Status = %s' % inner_solver.StatusName(inner_status))
+            print('Outer objective value =', outer.Objective().Value())
             # print('Number of solutions found: %i' % solution_printer.solution_count())
-
-            for i in range(V):
-                print('x[%i] = %i' % (i, inner_solver.Value(x[i])), end=', ')
-                print('y[%i] = %i' % (i, inner_solver.Value(y[i])), end=', ')
-                print('z[%i] = %i' % (i, inner_solver.Value(z[i])))
-                for j in range(i + 1, V):
-                    for k in range(6):
-                        print('b[%i][%i][%i] = %i' % (i, j, k, inner_solver.Value(b[i][j][k])))
+            print_solution(inner_solver, x, y, z, b, V)
+            check_correctness(inner_solver, x, y, z, b, v, V, W, H, D, data)
 
             return
 
 
 def run_linear_solver():
-    result = resolve_by_linear_solver("Data\\" + "0.csv")
+    result = resolve_by_linear_solver("Data\\" + "200.csv")
 
 
 if __name__ == '__main__':
     # run_cp_model()
     run_linear_solver()
-
