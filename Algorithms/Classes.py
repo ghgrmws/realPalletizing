ef206050e544e744021e38cd15541fff298968cd
ghgrmws <chenyang.zhang.chn@outlook.com>
2023-03-29 18:31:52 +0800

    蓝廋啊麻匪

--- Algorithms/Classes.py
+++ Algorithms/Classes.py
@@ -3,19 +3,48 @@ from collections import namedtuple
 point = namedtuple('point', ('x', 'y', 'z'))
 
 
-class Box:
-    def __init__(self, w, h, d, x=0, y=0, z=0):
+class Cube:
+    def __init__(self, d, w, h):
+        self.depth = d      # x
+        self.width = w      # y
+        self.height = h     # z
+        self.volume = d * w * h
+
+    def set_depth(self, d):
+        self.depth = d
+
+    def get_depth(self):
+        return self.depth
+
+    def set_width(self, w):
+        self.width = w
+
+    def get_width(self):
+        return self.width
+
+    def set_height(self, h):
+        self.height = h
+
+    def get_height(self):
+        return self.height
+
+    def set_(self, d, w, h):
+        self.depth = d
         self.width = w
         self.height = h
-        self.depth = d
+        self.volume = d * w * h
+
+
+class Box(Cube):
+    def __init__(self, d, w, h, x, y, z):
+        super().__init__(d, w, h)
         self.x = x
         self.y = y
         self.z = z
-        self.volume = w * h * d
 
     def __repr__(self):
-        return 'Box (with width = %i, height = %i, depth = %i) is placed at (%i, %i, %i)' % \
-            (self.width, self.height, self.depth, self.x, self.y, self.z)
+        return 'Box (with depth = %i, width = %i, height = %i) is placed at (%i, %i, %i)' % \
+            (self.depth, self.width, self.height, self.x, self.y, self.z)
 
     def __lt__(self, other):
         a, b, c = other.get_position()
@@ -32,7 +61,7 @@ class Box:
             return False
 
     def get_position(self):
-        return self.x, self.y, self.z
+        return point(x=self.x, y=self.y, z=self.z)
 
     def rotate(self):  # just support horizontal 90° rotation
         if self.width == self.depth:
@@ -47,23 +76,3 @@ class Box:
         self.x = x
         self.y = y
         self.z = z
-
-
-class Space:
-    def __init__(self, w, h, d):
-        self.width = w
-        self.height = h
-        self.depth = d
-        self.volume = w * h * d
-        self.legal_x = [0]
-        self.legal_y = [0]
-        self.legal_z = [0]
-        self.box_sequence = list()
-
-    def all_legal_points(self):
-        points = set()
-        for x in self.legal_x:
-            for y in self.legal_y:
-                for z in self.legal_z:
-                    points.add(point(x=x, y=y, z=z))
-        return points
---
+++ Algorithms/GeneticAlgorithm.py
@@ -0,0 +1,6 @@
+class Genetic:
+    def __init__(self, D, W, H, boxes):
+        self.depth = D
+        self.width = W
+        self.height = H
+        self.boxes = boxes
