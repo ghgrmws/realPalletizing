a7ff21234e80c3df9844f0220997a354d2920d47
zcy <chenyang.zhang.chn@outlook.com>
2023-04-18 15:19:45 +0800

    1

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from collections import namedtuple
 from datetime import datetime
-import numpy as np
 from Algorithms.GeneticAlgorithm import Genetic
 from Algorithms.Classes import Coordinate, Space
 from methods import get_data, generate_data
@@ -14,13 +13,14 @@ def test():
 
 
 def run():
+    date_form = '%m-%d %H-%M-%S'
     start = datetime.now()
     N, D, W, H, boxes = get_data("Data\\0.csv")  # 0.834807
     ga = Genetic(D, W, H, boxes)
     print('The utilization is %f' % ga.solve())
     print(datetime.now() - start)
     print(ga.check())
-    ga.save_solution("Data\\0.out")
+    ga.save_solution("Data\\(0)%s.out" % str(datetime.now().strftime(date_form)))
     return
 
 
